be934766d0ff4e42170852a3cccc3f2fcb08e8bd
Nix-Banayat <164652107+Nix-Banayat@users.noreply.github.com>
2024-06-11 20:31:39 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,40 +1,15 @@
-﻿namespace AddDays
+﻿using System.Security.Cryptography;
+
+namespace AddDays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TODO: Create Methods
             Console.Write("Enter a number between 1 to 7: ");
             int dayNumber = Convert.ToInt16(Console.ReadLine());
 
-            switch (dayNumber)
-            {
-                case 1:
-                    Console.WriteLine("The day is MONDAY");
-                    break;
-                case 2:
-                    Console.WriteLine("The day is TUESDAY");
-                    break;
-                case 3:
-                    Console.WriteLine("The day is WEDNESDAY");
-                    break;
-                case 4:
-                    Console.WriteLine("The day is THURSDAY");
-                    break;
-                case 5:
-                    Console.WriteLine("The day is FRIDAY");
-                    break;
-                case 6:
-                    Console.WriteLine("The day is SATURDAY");
-                    break;
-                case 7:
-                    Console.WriteLine("The day is SUNDAY");
-                    break;
-                default:
-                    Console.WriteLine("Invalid Input.");
-                    break;
-            }
+            Console.WriteLine (Days(dayNumber));
 
             Console.Write("Input number to add to the day: ");
             int dayToAdd = Convert.ToInt16(Console.ReadLine());
@@ -68,6 +43,15 @@
                     Console.WriteLine("Invalid Input.");
                     break;
             }
+        }
+
+        public static string Days(int dayNumber)
+        {
+
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string message = dayNumber >= 1 && dayNumber <= 7? $"The day is {days[dayNumber-1]}." : "Invalid Input!";
+
+            return message;
         }
     }
 }
